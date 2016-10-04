813a2cef960964df0f4ba2a80e95905735416f78
BaumiBaum <spam@j-baum.at>
2016-10-05 00:02:23 +0200

    Update

--- 2_Packages/mudskipper_mod/etc/mudskipper/Extensions/app-folders-mudskipper/check_apps.sh
+++ 2_Packages/mudskipper_mod/etc/mudskipper/Extensions/app-folders-mudskipper/check_apps.sh
@@ -23,7 +23,7 @@ sleep 2
 		if [ ! "$Folders" == "true" ]; then
 
 	                $DIR/enable_folders.sh
-        	        echo Folders=true >> $USERDIR/config
+        	        echo Folders=true >> $USERDIR/config &> /dev/null
 
                 fi
 
